8f382128527d2f8549b5e7256c6bc78d1feb2532
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-12-30 21:40:17 +0300

    Change /projects route to use new controllers

--- routes/projectsRoute.js
+++ routes/projectsRoute.js
@@ -3,11 +3,38 @@ const router = express.Router();
 
 const isLoggedIn = require('../middleware/isLoggedIn');
 
-const createGetController = require('../controllers/projects/create/get');
+const indexGetController = require('../controllers/projects/index/get');
+const createIndexGetController = require('../controllers/projects/create/index/get');
+const createFinishGetController = require('../controllers/projects/create/finish/get');
+
+const indexPostController = require('../controllers/projects/index/post');
+const createSavePostController = require('../controllers/projects/create/save/post');
 
 router.get(
+  '/',
+    isLoggedIn,
+    indexGetController
+);
+router.get(
   '/create',
-    createGetController
+    isLoggedIn,
+    createIndexGetController
+);
+router.get(
+  '/create/finish',
+    isLoggedIn,
+    createFinishGetController
+);
+
+router.post(
+  '/',  
+    isLoggedIn,
+    indexPostController
+);
+router.post(
+  '/create/save',
+    isLoggedIn,
+    createSavePostController
 );
 
 module.exports = router;

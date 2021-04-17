0d859fef5367e2f23ba1cdca1d4727a3c573fa74
bilgincan <canbilgin.f@gmail.com>
2021-04-17 23:37:45 +0200

    Frontend of the filters are done

--- public/js/projects/report/index.js
+++ public/js/projects/report/index.js
@@ -1,3 +1,38 @@
+const FILTERS = ['age', 'gender']
+
+// finds the true filter object from HTMLCollection
+function getFilter(DOMElements, filter){
+  for(elem of DOMElements){
+    if(elem.classList.contains(filter))
+      return elem;
+  }
+}
+
 window.onload = () => {
   listenForContentHeader(document); // Listen for content header buttons
+
+  document.addEventListener('click', event =>{
+    let clicked_on_filters = false;
+
+    for(filter of FILTERS){
+      if(event.target.classList.contains(filter)){
+        const domElements = document.getElementsByClassName('filter-options');
+        const htmlElem = getFilter(domElements, filter);
+        if(htmlElem.style.display == "none") htmlElem.style.display = "flex";
+        else htmlElem.style.display = "none";
+        clicked_on_filters = true;
+      }
+    }
+    // close other filters
+    if(clicked_on_filters){
+      for(filter of FILTERS){
+        if(!event.target.classList.contains(filter) && !event.target.parentNode.classList.contains(filter)){
+          const domElements = document.getElementsByClassName('filter-options');
+          getFilter(domElements,filter).style.display = "none";
+        }
+      }
+    }
+
+  })
+
 }
